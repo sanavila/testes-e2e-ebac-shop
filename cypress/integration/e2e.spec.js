066d023ce5aea9ab51e3dbd4eb5efa5a1272df98
sanavila <rute.leone@hotmail.com>
2023-05-05 09:37:10 -0300

    Reaproveitamento de commands com preenchimento de dados dentro do próprio teste

--- cypress/integration/e2e.spec.js
+++ cypress/integration/e2e.spec.js
@@ -1,6 +1,5 @@
 /// <reference types="cypress" />
-import listaProdutos from '../fixtures/produtos.json'
-import dadosCheckout from '../fixtures/dadosCheckout.json'
+import { faker } from '@faker-js/faker'
 context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
     beforeEach(() => {
         cy.visit('/')
@@ -8,7 +7,47 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
 
     it('Deve escolher os produtos, preencher o checkout e validar a compra', () => {
         cy.get('#primary-menu > .menu-item-629 > a').click()
-        cy.addProdutos(listaProdutos)
-        cy.preencherChekout(dadosCheckout)
+        cy.addProdutos(
+          "Abominable Hoodie",
+          "M",
+          "Green",
+          "2"
+        )
+        cy.addProdutos(
+          "Atlas Fitness Tank",
+          "S",
+          "Blue",
+          "3"
+        )
+        cy.visit("produtos")
+        cy.get('#primary-menu > .menu-item-629 > a').click()
+        cy.get(':nth-child(2) > .page-numbers').click()
+        cy.addProdutos(
+          "Balboa Persistence Tee",
+          "M",
+          "Green",
+          "3"
+        )
+        cy.addProdutos(
+          "Beaumont Summit Kit",
+          "S",
+          "Orange",
+          "1"
+        )
+        cy.preencherChekout(
+          faker.name.firstName(),
+          faker.name.lastName(),
+          "EBAC",
+          "Brasil",
+          "R.Campeão",
+          "Caucaia",
+          "Ceará",
+          "61642170",
+          "335566998877",
+          faker.internet.email()
+        )
+        //Resultado esperado
+        cy.get('[class="woocommerce-notice woocommerce-notice--success woocommerce-thankyou-order-received"]')
+        .contains('Obrigado. Seu pedido foi recebido.', { matchCase: false })
     })
 })
